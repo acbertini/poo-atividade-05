c45be6c52e6dbf5e4a5b62a476ed775cb552b667
acbertini <acbertini@gmail.com>
2018-05-11 19:35:51 -0300

    Alteração classe quiz

--- src/java/br/com/fatecpg/quiz/Quiz.java
+++ src/java/br/com/fatecpg/quiz/Quiz.java
@@ -1,23 +1,51 @@
 package br.com.fatecpg.quiz;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
  * @author AnaBertini
  */
 public class Quiz {
-    private static ArrayList<Questao> test;
+    private static ArrayList<Questao> questoes;
+    private static ArrayList<Questao> teste;
+    private Usuario usuario;
+    
+    
+    public static ArrayList<Questao> gerarQuestoes(){
+        if(questoes==null){
+            questoes = new ArrayList<>();
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+        }
+        return questoes;
+    }
     
     public static ArrayList<Questao> getTeste(){
-        if(test==null){
-            test = new ArrayList<>();
-            test.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            test.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            test.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            test.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            test.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+        Random rand = new Random();
+        for(int i=0; i<5; i++){
+            int n = rand.nextInt(15);
+            teste.add(questoes.get(n));
         }
-        return test;
+        return teste;
     }
+    
+//    int random = randomGenerator.nextInt(4);
+//            if (!numbers.contains(random)) {
+//                numbers.add(random);
+//            }
 }
--- web/quiz.jsp
+++ web/quiz.jsp
@@ -4,14 +4,28 @@
     Author     : AnaBertini
 --%>
 
+<%@page import="br.com.fatecpg.quiz.Questao"%>
+<%@page import="java.util.ArrayList"%>
+<%@page import="br.com.fatecpg.quiz.Quiz"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Quiz!</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Quiz!!!!</h1>
+        <form action = "home.jsp">
+            <% for (int i=0;i<Quiz.gerarQuestoes().size(); i++){
+                Questao q = Quiz.gerarQuestoes().get(i); %>
+                <h2><%=q.getPergunta()%></h2>
+                <input type="radio" name="<%=q.getAlternativas()%>" value="<%=q.getAlternativas()[0]%>"/><%=q.getAlternativas()[0]%>
+                <input type="radio" name="<%=q.getAlternativas()%>" value="<%=q.getAlternativas()[1]%>"/><%=q.getAlternativas()[1]%>
+                <input type="radio" name="<%=q.getAlternativas()%>" value="<%=q.getAlternativas()[2]%>"/><%=q.getAlternativas()[2]%>
+            <%}%>
+            <hr/>
+            <input type="submit" name="tested" value="Enviar" />
+        </form>
     </body>
 </html>
