41222bfdddcdb6d5fc7b64ae5bc2771c2b0066a4
acbertini <acbertini@gmail.com>
2018-05-12 15:19:51 -0300

    Atualização classes Quiz e Usuário

--- src/java/br/com/fatecpg/quiz/Quiz.java
+++ src/java/br/com/fatecpg/quiz/Quiz.java
@@ -1,6 +1,8 @@
 package br.com.fatecpg.quiz;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -8,44 +10,77 @@ import java.util.Random;
  * @author AnaBertini
  */
 public class Quiz {
-    private static ArrayList<Questao> questoes;
-    private static ArrayList<Questao> teste;
-    private Usuario usuario;
-    
-    
-    public static ArrayList<Questao> gerarQuestoes(){
-        if(questoes==null){
-            questoes = new ArrayList<>();
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
-            questoes.add(new Questao("1+2", "1", new String[]{"1", "2", "3"}));
+
+    public static ArrayList<Quiz> topDez = new ArrayList<>();
+    public static ArrayList<Quiz> ultimosDez = new ArrayList<>();
+    private ArrayList<Questao> test;
+
+    private Usuario user;
+    private Date dataRealizacao;
+    private double pontuacao;
+    private static Questao[] questoes = new Questao[]{
+        new Questao("pergunta 1", "resposta", new String[]{"1", "2", "3", "4"}),
+        new Questao("pergunta 2", "resposta", new String[]{"1", "2", "3", "4"}),
+        new Questao("pergunta 3", "resposta", new String[]{"1", "2", "3", "4"}),
+        new Questao("pergunta 4", "resposta", new String[]{"1", "2", "3", "4"}),
+        new Questao("pergunta 5", "resposta", new String[]{"1", "2", "3", "4"}),
+        new Questao("pergunta 6", "resposta", new String[]{"1", "2", "3", "4"}),
+        new Questao("pergunta 7", "resposta", new String[]{"1", "2", "3", "4"}),
+        new Questao("pergunta 8", "resposta", new String[]{"1", "2", "3", "4"}),
+        new Questao("pergunta 9", "resposta", new String[]{"1", "2", "3", "4"}),
+        new Questao("pergunta 10", "resposta", new String[]{"1", "2", "3", "4"}),
+        new Questao("pergunta 11", "resposta", new String[]{"1", "2", "3", "4"}),
+        new Questao("pergunta 12", "resposta", new String[]{"1", "2", "3", "4"}),};
+
+    public Quiz(Usuario user) {
+        this.user = user;
+        test = new ArrayList<>(10);
+        for (int i = 0; i < 10; i++) {
+            int rand;
+            do {
+                rand = (int) (Math.random() * questoes.length);
+            } while (test.contains(questoes[rand]));
+            test.add(questoes[rand]);
         }
-        return questoes;
     }
-    
-    public static ArrayList<Questao> getTeste(){
-        Random rand = new Random();
-        for(int i=0; i<5; i++){
-            int n = rand.nextInt(15);
-            teste.add(questoes.get(n));
+
+    public Usuario getUser() {
+        return this.user;
+    }
+
+    public Date getDataRealizacao() {
+        return dataRealizacao;
+    }
+
+    public double getPontuacao() {
+        return pontuacao;
+    }
+
+    public void finalizaTeste(double pontuacao) {
+        this.pontuacao = pontuacao;
+        this.dataRealizacao = Calendar.getInstance().getTime();
+        this.user.getTestesFinalizados().add(this);
+        ultimosDez.add(0, this);
+
+        if (ultimosDez.size() > 10) {
+            ultimosDez.remove(10);
         }
-        return teste;
+
+        int size = topDez.size();
+        if (size == 0) {
+            topDez.add(this);
+        } else {
+            int i = size;
+            while (i > 0 && this.pontuacao > ultimosDez.get(i - 1).pontuacao) {
+                i--;
+            }
+            if (i < 10) {
+                ultimosDez.add(i, this);
+            }
+            if (ultimosDez.size() > 10) {
+                ultimosDez.remove(10);
+            }
+        }
     }
-    
-//    int random = randomGenerator.nextInt(4);
-//            if (!numbers.contains(random)) {
-//                numbers.add(random);
-//            }
+
 }
--- src/java/br/com/fatecpg/quiz/Usuario.java
+++ src/java/br/com/fatecpg/quiz/Usuario.java
@@ -1,10 +1,44 @@
 package br.com.fatecpg.quiz;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author AnaBertini
  */
 public class Usuario {
-    private static String[] nome;
-    private static int[] pontuacao;
+
+    public static ArrayList<Usuario> lista = new ArrayList<>();
+    private String nome;
+    private final ArrayList<Quiz> testesFinalizados;
+
+    public Usuario(String nome) {
+        this.nome = nome;
+        this.testesFinalizados = new ArrayList<>();
+        lista.add(this);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String name) {
+        this.nome = name;
+    }
+
+    public ArrayList<Quiz> getTestesFinalizados() {
+        return testesFinalizados;
+    }
+
+    public double getMaiorPontuacao() {
+        if (testesFinalizados.isEmpty()) {
+            return 0;
+        }
+        double sum = 0;
+        for (Quiz q : testesFinalizados) {
+            sum += q.getPontuacao();
+        }
+        return sum / (double) testesFinalizados.size();
+    }
+
 }
